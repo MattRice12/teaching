f43a15dad5bffbe400cf802572477a073cf01955
Matt Rice <mattrice12@outlook.com>
2018-09-13 19:40:33 -0500

    week-4 exercises

---
+++ week-4/main.js
@@ -0,0 +1,76 @@
+//// Strings
+
+// 1) create a function the logs "Hello World"
+
+function hello() {
+  console.log("Hello World");
+}
+
+hello()
+
+
+// 2) create a function the logs "Hello ____" where your name is filled in
+
+// 3) create a function that return the above instead of logging it. Then log the result
+
+/* ___________________ */
+//// Numbers
+
+// 1) Create a function that adds two numbers together
+
+// 2) Create a function that squares a number
+
+// 3) Create a function that squares the sum of two numbers, using the above functions
+
+/* ___________________ */
+//// Arrays
+
+// 1.a) Create a function that takes an array of numbers and adds them together
+
+// 1.b) Do this with forEach
+
+// 1.c) Do this with reduce
+
+
+// 2.a) Create a function that finds the factorial of a number (4! === 4x3x2x1)
+
+// 2.b) Do this with forEach
+
+// 2.c) Do this with reduce???
+
+/* ___________________ */
+//// Strings
+
+// Create a function that determines whether a string is a palindrome (characters are in the same order forwards and backwards)
+  // - "bob"
+  // - "A but tuba"
+  // - "The glass is half full"
+
+/* ___________________ */
+//// Objects
+
+
+// Write a function with the following data that bounces a person if they are under certain ages
+
+// let matt   = { name: "Matt", age: 30 }
+// let bob    = { name: "Bob", age: 67 }
+// let timmy  = { name: "Timmy", age: 14 }
+
+
+/* ___________________ */
+
+//// Reduce into objects
+
+// Write a function with the following array of names ["Matt", "Eric", "Crystal", "Eric", "Eric", "Crystal"], and build an object with the person's name as the key, and the number of times their name shows up as the value
+
+// Write a function with the following string "George Washington, Adam West, Kanye West" and build the following result
+// {
+//   Washington: [ 'George' ],
+//   West: [ 'Adam', 'Kanye' ]
+// }
+
+
+/* ___________________ */
+//// Fizzbuzz
+
+// Write FizzBuzz for numbers 1 through 100
